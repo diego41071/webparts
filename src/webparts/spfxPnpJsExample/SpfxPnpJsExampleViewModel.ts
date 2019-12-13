56a6467bdf5d8de04ab656cd8615a205af2d99f4
diego41071 <alexander.diego.2007@hotmail.com>
2019-12-13 00:29:28 -0500

    correccion de enlaces

--- src/webparts/spfxPnpJsExample/SpfxPnpJsExampleViewModel.ts
+++ src/webparts/spfxPnpJsExample/SpfxPnpJsExampleViewModel.ts
@@ -28,155 +28,177 @@ export interface OrderListItem {
 }
 
 export default class PnPjsExampleViewModel {
-  public description: KnockoutObservable<string> = ko.observable("");
-  public newItemTitle: KnockoutObservable<string> = ko.observable("");
-  public newItemNumber: KnockoutObservable<string> = ko.observable("");
-  public items: KnockoutObservableArray<OrderListItem> = ko.observableArray([]);
+                 public description: KnockoutObservable<string> = ko.observable(
+                   ""
+                 );
+                 public newItemTitle: KnockoutObservable<
+                   string
+                 > = ko.observable("");
+                 public newItemNumber: KnockoutObservable<
+                   string
+                 > = ko.observable("");
+                 public items: KnockoutObservableArray<
+                   OrderListItem
+                 > = ko.observableArray([]);
 
-  public labelClass: string = styles.label;
-  public helloWorldClass: string = styles.spfxPnpJsExample;
-  public containerClass: string = styles.container;
-  public rowClass: string = `ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`;
-  public buttonClass: string = `ms-Button ${styles.button}`;
+                 public labelClass: string = styles.label;
+                 public helloWorldClass: string = styles.spfxPnpJsExample;
+                 public containerClass: string = styles.container;
+                 public rowClass: string = `ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`;
+                 public buttonClass: string = `ms-Button ${styles.button}`;
 
-  constructor(bindings: IPnPjsExampleBindingContext) {
-    this.description(bindings.description);
+                 constructor(bindings: IPnPjsExampleBindingContext) {
+                   this.description(bindings.description);
 
-    // When web part description is updated, change this view model's description.
-    bindings.shouter.subscribe(
-      (value: string) => {
-        this.description(value);
-      },
-      this,
-      "description"
-    );
+                   // When web part description is updated, change this view model's description.
+                   bindings.shouter.subscribe(
+                     (value: string) => {
+                       this.description(value);
+                     },
+                     this,
+                     "description"
+                   );
 
-    // call the load the items
-    this.getItems().then(items => {
-      this.items(items);
-    });
-  }
+                   // call the load the items
+                   this.getItems().then(items => {
+                     this.items(items);
+                   });
+                  //  this.getServices().then(items => {
+                  //    this.items(items);
+                  //  });
+                 }
 
-  /**
-   * Gets the items from the list
-   */
-  private getItems(): Promise<OrderListItem[]> {
-    return this.ensureList().then(list => {
-      // here we are using the getAs operator so that our returned value will be typed
-      return list.items
-        .select("Title", "OData__x0076_mo3", "OData__x0071_iy4")
-        .get<OrderListItem[]>();
-    });
-  }
+                 /**
+                  * Gets the items from the list
+                  */
+                 private getItems(): Promise<OrderListItem[]> {
+                   return this.ensureList().then(list => {
+                     // here we are using the getAs operator so that our returned value will be typed
+                     return list.items
+                       .select("Title", "OData__x0076_mo3", "OData__x0071_iy4")
+                       .get<OrderListItem[]>();
+                   });
+                 }
 
-  /**
-   * Adds an item to the list
-   */
-  // public addItem(): void {
-  //   if (this.newItemTitle() !== "") {
-  //     this.ensureList().then(list => {
-  //       // add the new item to the SharePoint list
-  //       list.items
-  //         .add({
-  //           Title: this.newItemTitle()
-  //         })
-  //         .then((iar: ItemAddResult) => {
-  //           // add the new item to the display
-  //           this.items.push({
-  //             Title: iar.data.Title,
-  //             _x0076_mo3: iar.data._x0076_mo3
-  //           });
+                //  private getServices(): Promise<OrderListItem[]> {
+                //    return this.ensureList().then(list => {
+                //      // here we are using the getAs operator so that our returned value will be typed
+                //      return list.items
+                //        .select("Title")
+                //        .get<OrderListItem[]>();
+                //    });
+                //  }
 
-  //           // clear the form
-  //           this.newItemTitle("");
-  //         });
-  //     });
-  //   }
-  // }
+                 /**
+                  * Adds an item to the list
+                  */
+                 // public addItem(): void {
+                 //   if (this.newItemTitle() !== "") {
+                 //     this.ensureList().then(list => {
+                 //       // add the new item to the SharePoint list
+                 //       list.items
+                 //         .add({
+                 //           Title: this.newItemTitle()
+                 //         })
+                 //         .then((iar: ItemAddResult) => {
+                 //           // add the new item to the display
+                 //           this.items.push({
+                 //             Title: iar.data.Title,
+                 //             _x0076_mo3: iar.data._x0076_mo3
+                 //           });
 
-  /**
-   * Deletes an item from the list
-   */
-  public deleteItem(data): void {
-    if (confirm("Are you sure you want to delete this item?")) {
-      this.ensureList()
-        .then(list => {
-          list.items
-            .getById(data.Id)
-            .delete()
-            .then(_ => {
-              this.items.remove(data);
-            });
-        })
-        .catch((e: Error) => {
-          alert(`There was an error deleting the item ${e.message}`);
-        });
-    }
-  }
+                 //           // clear the form
+                 //           this.newItemTitle("");
+                 //         });
+                 //     });
+                 //   }
+                 // }
 
-  /**
-   * Ensures the list exists and if it creates it adds some default example data
-   */
-  private ensureList(): Promise<List> {
-    return new Promise<List>((resolve, reject) => {
-      // use lists.ensure to always have the list available
-      sp.web.lists
-        .ensure("CatalogodeServicio")
-        .then((ler: ListEnsureResult) => {
-          if (ler.created) {
-            // we created the list on this call so let's add a column
-            ler.list.fields
+                 /**
+                  * Deletes an item from the list
+                  */
+                 public deleteItem(data): void {
+                   if (confirm("Are you sure you want to delete this item?")) {
+                     this.ensureList()
+                       .then(list => {
+                         list.items
+                           .getById(data.Id)
+                           .delete()
+                           .then(_ => {
+                             this.items.remove(data);
+                           });
+                       })
+                       .catch((e: Error) => {
+                         alert(
+                           `There was an error deleting the item ${e.message}`
+                         );
+                       });
+                   }
+                 }
 
-              .addText("Title, Url, Descripcion")
-              .then(_ => {
-                // and we will also add a few items so we can see some example data
-                // here we use batching
+                 /**
+                  * Ensures the list exists and if it creates it adds some default example data
+                  */
+                 private ensureList(): Promise<List> {
+                   return new Promise<List>((resolve, reject) => {
+                     // use lists.ensure to always have the list available
+                     sp.web.lists
+                       .ensure("CatalogodeServicio")
+                       .then((ler: ListEnsureResult) => {
+                         if (ler.created) {
+                           // we created the list on this call so let's add a column
+                           ler.list.fields
 
-                // create a batch
-                let batch = sp.web.createBatch();
+                             .addText("Title, Url, Descripcion")
+                             .then(_ => {
+                               // and we will also add a few items so we can see some example data
+                               // here we use batching
 
-                ler.list
-                  .getListItemEntityTypeFullName()
-                  .then(typeName => {
-                    ler.list.items.inBatch(batch).add(
-                      {
-                        Title: "Title 1"
-                      },
-                      typeName
-                    );
+                               // create a batch
+                               let batch = sp.web.createBatch();
 
-                    ler.list.items.inBatch(batch).add(
-                      {
-                        Title: "Title 2"
-                      },
-                      typeName
-                    );
+                               ler.list
+                                 .getListItemEntityTypeFullName()
+                                 .then(typeName => {
+                                   ler.list.items.inBatch(batch).add(
+                                     {
+                                       Title: "Title 1"
+                                     },
+                                     typeName
+                                   );
 
-                    ler.list.items.inBatch(batch).add(
-                      {
-                        Title: "Title 3"
-                      },
-                      typeName
-                    );
+                                   ler.list.items.inBatch(batch).add(
+                                     {
+                                       Title: "Title 2"
+                                     },
+                                     typeName
+                                   );
 
-                    // excute the batched operations
-                    batch
-                      .execute()
-                      .then(_ => {
-                        // all of the items have been added within the batch
+                                   ler.list.items.inBatch(batch).add(
+                                     {
+                                       Title: "Title 3"
+                                     },
+                                     typeName
+                                   );
 
-                        resolve(ler.list);
-                      })
-                      .catch(e => reject(e));
-                  })
-                  .catch(e => reject(e));
-              })
-              .catch(e => reject(e));
-          } else {
-            resolve(ler.list);
-          }
-        })
-        .catch(e => reject(e));
-    });
-  }
-}
+                                   // excute the batched operations
+                                   batch
+                                     .execute()
+                                     .then(_ => {
+                                       // all of the items have been added within the batch
+
+                                       resolve(ler.list);
+                                     })
+                                     .catch(e => reject(e));
+                                 })
+                                 .catch(e => reject(e));
+                             })
+                             .catch(e => reject(e));
+                         } else {
+                           resolve(ler.list);
+                         }
+                       })
+                       .catch(e => reject(e));
+                   });
+                 }
+               }
